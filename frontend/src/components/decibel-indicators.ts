b2952cf80641ff71acf25c7a19ee4842a29dc8df
Nóra Szepes <lordblendi@gmail.com>
2023-10-26 00:09:00 +0200

    add fetch

--- frontend/src/components/decibel-indicators.ts
+++ frontend/src/components/decibel-indicators.ts
@@ -1,38 +1,91 @@
+import { Task } from "@lit/task"
 import { css, html, LitElement, TemplateResult } from "lit"
 import { customElement, property } from "lit/decorators.js"
 
 @customElement("decibel-indicators")
 export class DecibelIndicators extends LitElement {
+    connectedCallback(): void {
+        super.connectedCallback()
+        this.fetchData()
+    }
+
+    @property({ type: Object })
+    data: Decibels = {
+        left: -1,
+        right: -1,
+        center: -1,
+        surroundLeft: -1,
+        surroundRight: -1,
+    }
+
+    fetchData(): void {
+        fetch("http://localhost:5000")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok")
+                }
+                return response.json()
+            })
+            .then((data) => {
+                this.data = data
+                console.log("Success:", data)
+            })
+            .catch((error) => {
+                console.error("Error:", error)
+            })
+    }
+
     static styles = css`
         ul {
             list-style-type: none;
-            background-color: red;
+            text-align: right;
+        }
+        li {
+            display: flex;
+            width: 100%;
+            justify-content: space-between;
+        }
+        span {
+            min-width: 20px;
             text-align: right;
         }
     `
 
-    @property({ type: Number })
-    left: number = -1
-
-    @property({ type: Number })
-    right: number = -1
-
-    @property({ type: Number })
-    center: number = -1
-
-    @property({ type: Number })
-    surroundLeft: number = -1
-
-    @property({ type: Number })
-    surroundRight: number = -1
+    private _getDataTask = new Task(this, {
+        task: async (): Promise<Decibels> => {
+            console.log("nora")
+            const response = await fetch("example.com/data/")
+            return response.json()
+        },
+    })
 
     render(): TemplateResult {
         return html`<ul>
-            <li>L: ${this.left} dB</li>
-            <li>R: ${this.right} dB</li>
-            <li>C: ${this.center} dB</li>
-            <li>SL: ${this.surroundLeft} dB</li>
-            <li>SR: ${this.surroundRight} dB</li>
+            <li>
+                L:&nbsp;
+                <span>${this.data.left}</span>
+                &nbsp;dB
+            </li>
+            <li>
+                R:&nbsp;
+                <span>${this.data.right}</span>
+                &nbsp;dB
+            </li>
+            <li>
+                C:&nbsp;
+                <span>${this.data.center}</span>
+                &nbsp;dB
+            </li>
+            <li>
+                SL:&nbsp;
+                <span>${this.data.surroundLeft}</span>
+                &nbsp;dB
+            </li>
+            <li>
+                SR:&nbsp;
+                <span>${this.data.surroundRight}</span>
+                &nbsp;dB
+            </li>
         </ul>`
     }
 }
